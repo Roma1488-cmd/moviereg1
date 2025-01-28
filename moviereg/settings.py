8334999468ca773a436a46da9f3a3ff984994447
Roma1488-cmd <78442328+Roma1488-cmd@users.noreply.github.com>
2025-01-28 13:07:49 +0300

    Add files via upload

--- moviereg/settings.py
+++ moviereg/settings.py
@@ -109,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+...
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -124,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Це директорія, куди будуть зібрані
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
