d30a3f8b82be2e661d411ecbab736d90235a8ec6
Roma1488-cmd <78442328+Roma1488-cmd@users.noreply.github.com>
2025-01-29 06:51:48 +0300

    Add files via upload

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import DelayMessage, ScheduledMessage, SubscribeChannel, TelegramUser, UserCode
 
-# Register your models here.
+admin.site.register(DelayMessage)
+admin.site.register(ScheduledMessage)
+admin.site.register(SubscribeChannel)
+admin.site.register(TelegramUser)
+admin.site.register(UserCode)
--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class DelayMessage(models.Model):
+    message = models.TextField("Message")
+    delay = models.DurationField("Delay")
+
+class ScheduledMessage(models.Model):
+    message = models.TextField("Message")
+    scheduled_time = models.DateTimeField("Scheduled time")
+
+class SubscribeChannel(models.Model):
+    channel_name = models.CharField("Channel Name", max_length=255)
+    subscribe_link = models.URLField("Subscribe Link")
+
+class TelegramUser(models.Model):
+    user_id = models.CharField("User ID", max_length=255)
+    username = models.CharField("Username", max_length=255)
+
+class UserCode(models.Model):
+    code = models.CharField("Code", max_length=255)
+    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE, related_name="codes")
