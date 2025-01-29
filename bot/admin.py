e33bf6d6303389619b67a1bd1c0fb30611d72e3a
Roma1488-cmd <78442328+Roma1488-cmd@users.noreply.github.com>
2025-01-29 09:12:19 +0300

    Add files via upload

--- bot/admin.py
+++ bot/admin.py
@@ -1,39 +1,87 @@
 from django.contrib import admin
+from django.contrib import messages
 from solo.admin import SingletonModelAdmin
-from .models import BotConfiguration
+from .models import BotConfiguration, DelayMessage, ScheduledMessage, TelegramUser
+import telebot
 
 
 @admin.register(BotConfiguration)
 class BotConfigurationAdmin(SingletonModelAdmin):
     fieldsets = [
-        (None, {"fields": ['start_message', 'subscribe_message', 'subscribe_button_text', 'subscribe_button_link',
-                           'admin_chat_id', 'android_app_link']}),
-        ("Delay Message", {"classes": ['collapse'],
-                           "fields": ['is_send_delay_message', 'media', 'media_file', 'message', 'button_text',
-                                      'button_link', 'delay_seconds', 'media_id']})
+        (None, {"fields": ['channel_id', 'admin_chat_id']}),
     ]
-    readonly_fields = ['media_id']
 
     def save_model(self, request, instance, form, change):
-        if not instance.is_send_delay_message:
-            super().save_model(request, instance, form, change)
-            return
-
         bot = telebot.TeleBot("7283206544:AAHQzhdTykJwtGmqLvkS4tY8uR_QhI45XhQ")
-        chat_id = BotConfiguration.get_solo().admin_chat_id
-        text = instance.message
-        file = instance.media_file
+        admin_chat_id = instance.admin_chat_id
+
         try:
-            if instance.media == "photo":
-                message = bot.send_photo(chat_id=chat_id, caption=text, photo=file)
-                instance.media_id = message.photo[0].file_id
-            elif instance.media == "video":
-                message = bot.send_video(chat_id=chat_id, caption=text, video=file)
-                instance.media_id = message.video.file_id
-            else:
-                bot.send_message(chat_id=chat_id, text=text)
+            bot.send_message(chat_id=admin_chat_id, text="Тестове повідомлення")  # Відправка тестового повідомлення
         except Exception as error:
             messages.add_message(request, messages.ERROR, str(error))
             return
 
         super().save_model(request, instance, form, change)
+
+
+@admin.register(DelayMessage)
+class DelayMessageAdmin(admin.ModelAdmin):
+    list_display = ('message_type', 'text', 'scheduled_time', 'is_send')
+    fieldsets = [
+        (None, {"fields": ['message_type', 'text', 'media_file', 'scheduled_time', 'button_text', 'button_link',
+                           'additional_media', 'is_send']}),
+    ]
+    readonly_fields = ('media_id',)
+
+    def save_model(self, request, instance, form, change):
+        bot = telebot.TeleBot("7283206544:AAHQzhdTykJwtGmqLvkS4tY8uR_QhI45XhQ")
+        try:
+            if instance.message_type == "photo" and instance.media_file:
+                bot.send_photo(chat_id=BotConfiguration.get_solo().admin_chat_id, photo=instance.media_file,
+                               caption=instance.text)
+                instance.media_id = message.photo[0].file_id
+            elif instance.message_type == "video" and instance.media_file:
+                bot.send_video(chat_id=BotConfiguration.get_solo().admin_chat_id, video=instance.media_file,
+                               caption=instance.text)
+                instance.media_id = message.video.file_id
+            elif instance.message_type == "text":
+                bot.send_message(chat_id=BotConfiguration.get_solo().admin_chat_id, text=instance.text)
+        except Exception as error:
+            messages.add_message(request, messages.ERROR, str(error))
+            return
+        super().save_model(request, instance, form, change)
+
+
+@admin.register(ScheduledMessage)
+class ScheduledMessageAdmin(admin.ModelAdmin):
+    list_display = ('message_type', 'text', 'scheduled_time', 'is_send')
+    fieldsets = [
+        (None, {"fields": ['message_type', 'text', 'media_file', 'scheduled_time', 'button_text', 'button_link',
+                           'additional_media', 'is_send']}),
+    ]
+    readonly_fields = ('media_id',)
+
+    def save_model(self, request, instance, form, change):
+        bot = telebot.TeleBot("7283206544:AAHQzhdTykJwtGmqLvkS4tY8uR_QhI45XhQ")
+        channel_id = BotConfiguration.get_solo().channel_id
+        try:
+            if instance.message_type == "photo" and instance.media_file:
+                bot.send_photo(chat_id=channel_id, photo=instance.media_file, caption=instance.text)
+                instance.media_id = message.photo[0].file_id
+            elif instance.message_type == "video" and instance.media_file:
+                bot.send_video(chat_id=channel_id, video=instance.media_file, caption=instance.text)
+                instance.media_id = message.video.file_id
+            elif instance.message_type == "text":
+                bot.send_message(chat_id=channel_id, text=instance.text)
+        except Exception as error:
+            messages.add_message(request, messages.ERROR, str(error))
+            return
+        super().save_model(request, instance, form, change)
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ('chat_id', 'username', 'first_name', 'language_code', 'created_at')
+    list_filter = ('created_at', 'language_code')
+    search_fields = ('chat_id', 'username', 'first_name')
+    readonly_fields = ('chat_id', 'username', 'first_name', 'language_code', 'created_at')
--- bot/models.py
+++ bot/models.py
@@ -1,18 +1,66 @@
 from django.db import models
-from solo.models import SingletonModel
+from django.utils import timezone  # У першу допомогу імпорт додано
+from solo.models import SingletonModel  # Переконайтеся, що 'SingletonModel' імпортовано
 
 class BotConfiguration(SingletonModel):
-    start_message = models.TextField("Start message")
-    subscribe_message = models.TextField("Subscribe message")
-    subscribe_button_text = models.CharField("Subscribe button text", max_length=255)
-    subscribe_button_link = models.URLField("Subscribe button link")
-    admin_chat_id = models.CharField("Admin chat id", max_length=255)
-    android_app_link = models.URLField("Android app link")
-    is_send_delay_message = models.BooleanField("Send delay message", default=False)
-    delay_seconds = models.PositiveIntegerField("Delay seconds", default=0)
-    media = models.CharField("Media type", max_length=10, choices=[('photo', 'Photo'), ('video', 'Video')])
-    media_file = models.FileField("Media file", upload_to='media_files')
-    message = models.TextField("Message")
-    button_text = models.CharField("Button text", max_length=255, blank=True, null=True)
-    button_link = models.URLField("Button link", blank=True, null=True)
+    channel_id = models.CharField("Channel ID", max_length=255, default="")
+    admin_chat_id = models.CharField("Admin Chat ID", max_length=255)
+
+    class Meta:
+        verbose_name = "Bot Configuration"
+        verbose_name_plural = "Bot Configurations"
+
+    def __str__(self):
+        return self.channel_id
+
+class DelayMessage(models.Model):
+    bot_configuration = models.ForeignKey(BotConfiguration, on_delete=models.CASCADE, related_name="delay_messages")
+    message_type = models.CharField("Message Type", max_length=5, choices=[('text', 'Text'), ('photo', 'Photo'), ('video', 'Video')])
+    text = models.TextField("Text", blank=True, null=True)
+    media_file = models.FileField("Media File", upload_to='media_files', blank=True, null=True)
+    scheduled_time = models.DateTimeField("Scheduled Time", default=timezone.now)
+    button_text = models.CharField("Button Text", max_length=100, blank=True, null=True)
+    button_link = models.URLField("Button Link", blank=True, null=True)
+    additional_media = models.FileField("Additional Media", upload_to='media_files', blank=True, null=True)
+    is_send = models.BooleanField("Send", default=False)
     media_id = models.CharField("Media ID", max_length=255, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Delay Message"
+        verbose_name_plural = "Delay Messages"
+
+    def __str__(self):
+        return f"{self.message_type} message scheduled for {self.scheduled_time}"
+
+class ScheduledMessage(models.Model):
+    bot_configuration = models.ForeignKey(BotConfiguration, on_delete=models.CASCADE, related_name="scheduled_messages")
+    message_type = models.CharField("Message Type", max_length=5, choices=[('text', 'Text'), ('photo', 'Photo'), ('video', 'Video')])
+    text = models.TextField("Text", blank=True, null=True)
+    media_file = models.FileField("Media File", upload_to='media_files', blank=True, null=True)
+    scheduled_time = models.DateTimeField("Scheduled Time", default=timezone.now)
+    button_text = models.CharField("Button Text", max_length=100, blank=True, null=True)
+    button_link = models.URLField("Button Link", blank=True, null=True)
+    additional_media = models.FileField("Additional Media", upload_to='media_files', blank=True, null=True)
+    is_send = models.BooleanField("Send", default=False)
+    media_id = models.CharField("Media ID", max_length=255, blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Scheduled Message"
+        verbose_name_plural = "Scheduled Messages"
+
+    def __str__(self):
+        return f"{self.message_type} message scheduled for {self.scheduled_time}"
+
+class TelegramUser(models.Model):
+    chat_id = models.CharField("Chat ID", max_length=255)
+    username = models.CharField("Username", max_length=255)
+    first_name = models.CharField("First Name", max_length=255)
+    language_code = models.CharField("Language Code", max_length=10, blank=True, null=True)
+    created_at = models.DateTimeField("Created At", default=timezone.now)
+
+    class Meta:
+        verbose_name = "Telegram User"
+        verbose_name_plural = "Telegram Users"
+
+    def __str__(self):
+        return self.chat_id
