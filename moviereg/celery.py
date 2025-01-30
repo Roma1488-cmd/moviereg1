30f7bd51bc98a03e22ee9b06328d205d549cf4bf
Roma1488-cmd <78442328+Roma1488-cmd@users.noreply.github.com>
2025-01-30 08:54:21 +0300

    Add files via upload

--- moviereg/celery.py
+++ moviereg/celery.py
@@ -1,7 +1,6 @@
 import os
 from celery import Celery
 from dotenv import load_dotenv
-import ssl
 
 load_dotenv()  # Завантаження змінних середовища з файлу .env
 
@@ -13,23 +12,15 @@ redis_url = os.getenv('REDIS_URL')
 if not redis_url:
     raise ValueError("REDIS_URL не налаштована у середовищі")
 
-# Налаштування Celery для використання Redis як брокера та бекенду з параметрами SSL
-ssl_context = ssl.create_default_context()
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
-
+# Налаштування Celery для використання Redis як брокера та бекенду
 ssl_options = {
-    'ssl_cert_reqs': ssl.CERT_NONE
+    'ssl_cert_reqs': os.getenv('SSL_CERT_REQS', 'CERT_NONE')
 }
 app.conf.update(
     broker_url=redis_url,
     result_backend=redis_url,
-    broker_use_ssl={
-        'ssl_cert_reqs': ssl.CERT_NONE
-    },
-    redis_backend_use_ssl={
-        'ssl_cert_reqs': ssl.CERT_NONE
-    },
+    broker_use_ssl=ssl_options,
+    redis_backend_use_ssl=ssl_options,
     broker_connection_retry_on_startup=True
 )
 
